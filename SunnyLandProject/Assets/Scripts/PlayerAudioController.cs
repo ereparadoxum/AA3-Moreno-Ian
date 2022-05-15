444d363e5b995d0a352628bee04751bcd0a5a762
ereparadoxum <freddyian63@gmail.com>
2022-05-15 15:55:58 +0200

    AA3 MORENO IAN

--- SunnyLandProject/Assets/Scripts/PlayerAudioController.cs
+++ SunnyLandProject/Assets/Scripts/PlayerAudioController.cs
@@ -4,58 +4,106 @@ using UnityEngine;
 
 public class PlayerAudioController : MonoBehaviour
 {
+
+    AudioSource[] allSources;
+
+    AudioSource SaltarSource;
+    AudioSource AterrizarSource;
+    AudioSource AgacharseSource;
+    AudioSource CerezaSource;
+    AudioSource CorrerSource;
+
     // keep track of the jumping state ... 
     bool isJumping = false;
     // make sure to keep track of the movement as well !
 
     Rigidbody2D rb; // note the "2D" prefix 
+
+    bool isPlaying = false;
     
     // Start is called before the first frame update
     void Start()
     {
+
 	rb = GetComponent<Rigidbody2D>();
-	// get the references to your audio sources here !        
+
+	// get the references to your audio sources here !  
+
+    allSources = GetComponents<AudioSource>();
+
+    AterrizarSource = allSources[0];
+    SaltarSource = allSources[1];
+    AgacharseSource = allSources[2];
+    CerezaSource = allSources[3];
+    CorrerSource = allSources[4];
+
     }
 
     // FixedUpdate is called whenever the physics engine updates
     void FixedUpdate()
     {
-	// Use the ridgidbody instance to find out if the fox is
-	// moving, and play the respective sound !
-	// Make sure to trigger the movement sound only when
-	// the movement begins ...
 
-	// Use a magnitude threshold of 1 to detect whether the
-	// fox is moving or not !
-	// i.e.
-	// if ( ??? > 1 && ???) {
-	//    play sound here !
-	// } else if ( ??? < 1 &&) {
-	//   stop sound here !
-	// }	
+    float v = rb.velocity.magnitude;
+
+    if (v > 1 && !isPlaying) {
+        CorrerSource.Play();
+        isPlaying = true;
+    }
+    else if (v < 1 && isPlaying) {
+        CorrerSource.Stop();
+        isPlaying = false;
+    }
+    if (isJumping) {
+        CorrerSource.Stop();
+    }
+
     }
     
     // trigger your landing sound here !
     public void OnLanding() {
+
+        int x = Random.Range(1,100);
+    if (x<=50){
+        AterrizarSource.pitch=1;
+        AterrizarSource.Play();
+    }
+    else if (x>=50){
+        AterrizarSource.pitch=x/20;
+        AterrizarSource.Play();
+    }
+
         isJumping = false;
         print("the fox has landed");
-	// to keep things cleaner, you might want to
-	// play this sound only when the fox actually jumoed ...
     }
 
     // trigger your crouching sound here
     public void OnCrouching() {
         print("the fox is crouching");
+
+        AgacharseSource.Play();
     }
  
     // trigger your jumping sound here !
     public void OnJump() {
+        int x = Random.Range(1,100);
         isJumping = true;
         print("the fox has jumped");
+
+    if (x<=50){
+        SaltarSource.pitch=1;
+        SaltarSource.Play();
+    }
+    else if (x>=50){
+        SaltarSource.pitch=x/20;
+        SaltarSource.Play();
+    }
+    
     }
 
     // trigger your cherry collection sound here !
     public void OnCherryCollect() {
         print("the fox has collected a cherry");
+
+        CerezaSource.Play();
     }
 }
